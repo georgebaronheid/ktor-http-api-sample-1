8391566964985e4c444b9ae1f80e8dccb88dd249
George Baronheid <baronheid.george@gmail.com>
2021-05-19 19:46:38 -0300

    Went until setting registering customer routes

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,21 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
+import com.jetbrains.handson.httpapi.routes.registerCustomerRoutes
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.serialization.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
+    /**
+     * This provides a tool for the server to inspect the header argument from a request and check if
+     * it's accept argument is what we can provide.
+     */
+    install(ContentNegotiation) {
+        json()
+    }
+//    Our routing that was set @{CustomerRoutes.kt}
+    registerCustomerRoutes()
 }
---
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
@@ -0,0 +1,53 @@
+package com.jetbrains.handson.httpapi.routes
+
+import com.jetbrains.handson.httpapi.models.Customer
+import com.jetbrains.handson.httpapi.models.customerStorage
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {
+            if (customerStorage.isNotEmpty()) call.respond(customerStorage)
+            else call.respondText("No customers found", status = HttpStatusCode.NotFound)
+        }
+        get("{ìd}") {
+//                First check if parameter exists
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Missing or malformed id",
+                status = HttpStatusCode.BadRequest
+            )
+//                if not null but doesn't exists 404
+            val customer = customerStorage.find { it.id == id } ?: return@get call.respondText(
+                "No customer with id $id",
+                status = HttpStatusCode.NotFound
+            )
+            call.respond(customer)
+
+        }
+        post {
+            val customer = call.receive<Customer>()
+            customerStorage.add(customer)
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
+
+        }
+        delete("{id}") {
+            val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
+            if (customerStorage.removeIf { it.id == id }) call.respondText(
+                "Customer removed correctly",
+                status = HttpStatusCode.Accepted
+            )
+            else call.respondText("Not found", status = HttpStatusCode.NotFound)
+        }
+    }
+}
+
+fun Application.registerCustomerRoutes() {
+    routing {
+        customerRouting()
+    }
+}
